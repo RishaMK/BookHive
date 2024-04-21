77f11dd7d127b3ebd64ace2f22d0b4aef7d84a78
Risha <rishamkeerthi@gmail.com>
2024-04-21 23:43:08 +0530

    homepage to list all books

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,10 +3,80 @@ import axios from 'axios';
 import Spinner from '../components/Spinner';
 import { Link } from 'react-router-dom';
 import { AiOutlineEdit } from 'react-icons/ai';
+import { BsInfoCircle } from 'react-icons/bs';
+import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
 
 const Home = () => {
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get('http://localhost:5555/books')
+      .then((response) => {
+        setBooks(response.data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  }, []);
   return (
-    <div>Home</div>
+    <div className='p-4'>
+      <div className='flex justify-between items-center'>
+        <h1 className='text-3xl my-8'>Books List</h1>
+        <Link to='/books/create'>
+          <MdOutlineAddBox className='text-sky-800 text-4xl' />
+        </Link>
+      </div>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <table className='w-full border-none'>
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Title</th>
+              <th className=' max-md:hidden'>Author</th>
+              <th className=' max-md:hidden'>Published Year</th>
+              <th>Operations</th>
+            </tr>
+          </thead>
+          <tbody>
+            {books.map((book, index) => (
+              <tr key={book._id} className='h-8'>
+                <td className='text-center py-4'>
+                  {index + 1}
+                </td>
+                <td className='text-center py-4'>
+                  {book.title}
+                </td>
+                <td className='text-center max-md:hidden py-4'>
+                  {book.author}
+                </td>
+                <td className='text-center max-md:hidden py-4'>
+                  {book.published}
+                </td>
+                <td className='text-center py-4'>
+                  <div className='flex justify-center gap-x-4'>
+                    <Link to={`/books/details/${book._id}`}>
+                      <BsInfoCircle className='text-2xl text-green-800' />
+                    </Link>
+                    <Link to={`/books/edit/${book._id}`}>
+                      <AiOutlineEdit className='text-2xl text-yellow-600' />
+                    </Link>
+                    <Link to={`/books/delete/${book._id}`}>
+                      <MdOutlineAddBox className='text-2xl text-red-600'></MdOutlineAddBox>
+                    </Link>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
   )
 }
 

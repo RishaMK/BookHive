8afe982e614a11defbed5286e92f79e2e2389e9f
Risha <rishamkeerthi@gmail.com>
2024-04-24 00:02:59 +0530

    made the info and add books page

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -1,8 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { useSnackbar } from 'notistack';
 
 const createBook = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [published, setPublished] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate(); //unlike Link, this is used to navigate based on certain triggers or events
+  const { enqueueSnackbar } = useSnackbar();
+
+  const handleSaveBook = () => {
+    const data = {
+      title,
+      author,
+      published
+    };
+    setLoading(true);
+    axios
+      .post('http://localhost:5555/books', data)
+      .then(() => {
+        setLoading(false);
+        navigate('/');
+      })
+      .catch((error)=>{
+        setLoading(false);
+        alert('error occured. check console');
+        console.log(error);
+      });
+  }
+
   return (
-    <div>createBook</div>
+    <div className='p-4'>
+      <BackButton />
+      <h1 className='text-3xl my-4'>Create Book</h1>
+      {loading ? (
+        <Spinner />
+      ):(
+        <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
+          <div className='my-4'>
+            <label className='text-xl mr-4 text-gray-500'>Title</label>
+            <input 
+              type='text'
+              value={title}
+              onChange={(e)=>{setTitle(e.target.value)}}
+              className='border-2 borer-gray-500 px-4 py-2 w-full'
+            />
+          </div>
+          <div className='my-4'>
+            <label className='text-xl mr-4 text-gray-500'>Author</label>
+            <input 
+              type='text'
+              value={author}
+              onChange={(e)=>{setAuthor(e.target.value)}}
+              className='border-2 borer-gray-500 px-4 py-2 w-full'
+            />
+          </div>
+          <div className='my-4'>
+            <label className='text-xl mr-4 text-gray-500'>Published Year</label>
+            <input 
+              type='text'
+              value={published}
+              onChange={(e)=>{setPublished(e.target.value)}}
+              className='border-2 borer-gray-500 px-4 py-2 w-full'
+            />
+          </div>
+          <button className='p-2 bg-sky-300 m-8' onClick={handleSaveBook}>
+            Save
+          </button>
+        </div>
+      )}
+    </div>
   )
 }
 

7bfb67a0320378a55ef919eb90f61acfe813dbab
Risha <rishamkeerthi@gmail.com>
2024-05-01 12:09:38 +0530

    config

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,3 @@
 export const PORT = 5555;
 
-export const mongoDBURL = 'mongodb+srv://adminroot:adminroot@bookstore.c0uyknp.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookStore'
-
-//database deleted
+export const mongoDBURL = "mongodb+srv://adminroot:adminroot@bookstore.c0uyknp.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookStore"
